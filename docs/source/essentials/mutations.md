6a2ba735a44f0a6929220b7bc7a50144066afb5b
Shadaj Laddad <shadaj@users.noreply.github.com>
2018-03-28 11:20:35 -0700

    Add support for query options and start queries docs

---
+++ docs/source/essentials/mutations.md
@@ -0,0 +1,82 @@
+---
+title: Mutations
+order: 3
+---
+
+Now that you have your build set up, let's start writing our first GraphQL application! 
+
+## Create a client
+First, we need to create an instance of Apollo Client, which will handle sending our GraphQL queries and caching the results (among many other features).
+
+Inside your main function, we can use Apollo Boost to set up a client with a default HTTP link and in-memory cache.
+```scala
+import com.apollographql.scalajs._
+
+def main(): Unit = { // called when the app launches
+  val client = ApolloBoostClient(
+    uri = "https://w5xlvm3vzz.lp.gql.zone/graphql"
+  )
+}
+```
+
+That’s it! Now your client is ready to start fetching data. Before we hook up Apollo Scala.js to React, let’s try sending a query with plain Scala first with the `client.query` function.
+
+```scala
+client.query(gql( // gql is a member of the com.apollographql.scalajs package that parses your query
+  """{
+    |  rates(currency: "USD") {
+    |    currency
+    |  }
+    |}""".stripMargin
+))).foreach { result =>
+  println(result.data)
+}
+```
+
+If you open up the console, you should see the result of your GraphQL query. Now, let's learn how to connect Apollo Scala.js to React so we can start building query components with React Apollo.
+
+## Connecting your client to React
+To connect Apollo Client to React, you will need to use the `ApolloProvider` component, which can be found in the `com.apollographql.scalajs.react` package. For details on what the `ApolloProvider` component does, see the [React Apollo Docs](https://www.apollographql.com/docs/react/essentials/get-started.html#creating-provider). We suggest placing your `ApolloProvider` somewhere high in your app, above any places where you need to access GraphQL data.
+
+```scala
+import com.apollographql.scalajs.react.ApolloProvider
+
+ReactDOM.render(
+  ApolloProvider(client)(
+    ...
+  ),
+  rootElement
+)
+```
+
+## Request data
+Now that you have an `ApolloProvider` mounted, it's time to start performing queries with the `Query` component!
+
+First, specify the type of your query result, then pass in your `gql`-parsed GraphQL query and a function that determines what to render as a curried parameter. To keep things simple, let's leave our component untyped by specifying `js.Object` as the return type.
+
+The curried parameter is a function that takes the current query resolution result and returns a React tree. Because we don't always have the data requested, the `data` property on the result object is an `Option`.
+
+```scala
+import com.apollographql.scalajs.react.Query
+
+Query[js.Object](gql(
+  """{
+    |  rates(currency: "USD") {
+    |    currency
+    |  }
+    |}""".stripMargin
+))) { result =>
+  if (result.loading) {
+    h1("Loading!")
+  } else {
+    div(result.data.get.toString)
+  }
+}
+```
+
+If you include this component in your React tree, you should see your GraphQL query result rendered to your screen.
+
+## Next steps
+Now that you’ve learned how to fetch data with Apollo Scala.js, you’re ready to dive deeper into creating more complex queries and mutations. After this section, we recommend moving onto:
++ [Queries](/essentials/queries.html): Learn how to fetch queries with arguments and handle the results with static types
++ [Mutations](/essentials/mutations.html): Learn how to update data with mutations and type your parameters and results
--- react/src/main/scala/com/apollographql/scalajs/react/Query.scala
+++ react/src/main/scala/com/apollographql/scalajs/react/Query.scala
@@ -22,24 +22,28 @@ object QueryData {
   }
 }
 
+case class QueryOptions(pollInterval: js.UndefOr[Double] = js.undefined,
+                        notifyOnNetworkStatusChange: js.UndefOr[Boolean] = js.undefined,
+                        fetchPolicy: js.UndefOr[String] = js.undefined,
+                        errorPolicy: js.UndefOr[String] = js.undefined,
+                        ssr: js.UndefOr[Boolean] = js.undefined,
+                        displayName: js.UndefOr[String] = js.undefined,
+                        delay: js.UndefOr[Boolean] = js.undefined,
+                        context: js.UndefOr[js.Object] = js.undefined)
+
 object Query extends ExternalComponent {
   case class Props(query: ParsedQuery,
+                   children: js.Object => ReactElement,
                    variables: js.UndefOr[js.Object] = js.undefined,
-                   children: js.Object => ReactElement)
-
-  def apply[T](query: ParsedQuery)
-              (children: QueryData[T] => ReactElement)
-              (implicit tReader: Reader[T]): slinky.core.BuildingComponent[Element, js.Object] = {
-    val queryDataReader = QueryData.reader(tReader)
-    apply(Props(
-      query = query,
-      children = d => {
-        children(queryDataReader.read(d))
-      }
-    ))
-  }
-
-  def apply[T, V](query: ParsedQuery, variables: V)
+                   pollInterval: js.UndefOr[Double] = js.undefined,
+                   notifyOnNetworkStatusChange: js.UndefOr[Boolean] = js.undefined,
+                   fetchPolicy: js.UndefOr[String] = js.undefined,
+                   errorPolicy: js.UndefOr[String] = js.undefined,
+                   ssr: js.UndefOr[Boolean] = js.undefined,
+                   displayName: js.UndefOr[String] = js.undefined,
+                   delay: js.UndefOr[Boolean] = js.undefined,
+                   context: js.UndefOr[js.Object] = js.undefined)
+  def apply[T, V](query: ParsedQuery, variables: V, queryOptions: QueryOptions)
                  (children: QueryData[T] => ReactElement)
                  (implicit tReader: Reader[T], vWriter: Writer[V]): slinky.core.BuildingComponent[Element, js.Object] = {
     val queryDataReader = QueryData.reader(tReader)
@@ -48,34 +52,87 @@ object Query extends ExternalComponent {
       variables = vWriter.write(variables),
       children = d => {
         children(queryDataReader.read(d))
-      }
+      },
+      // queryOptions
+      pollInterval = queryOptions.pollInterval,
+      notifyOnNetworkStatusChange = queryOptions.notifyOnNetworkStatusChange,
+      fetchPolicy = queryOptions.fetchPolicy,
+      errorPolicy = queryOptions.errorPolicy,
+      ssr = queryOptions.ssr,
+      displayName = queryOptions.displayName,
+      delay = queryOptions.delay,
+      context = queryOptions.context
     ))
   }
 
-  def apply(query: GraphQLQuery { type Variables = Unit })
-           (children: QueryData[query.Data] => ReactElement)
-           (implicit dataReader: Reader[query.Data]): slinky.core.BuildingComponent[Element, js.Object] = {
-    val queryDataReader = QueryData.reader(dataReader)
-    apply(Props(
+  def apply[T, V](query: ParsedQuery, variables: V)
+                 (children: QueryData[T] => ReactElement)
+                 (implicit tReader: Reader[T], vWriter: Writer[V]): slinky.core.BuildingComponent[Element, js.Object] = {
+    apply[T, V](
+      query = query,
+      variables = variables,
+      queryOptions = QueryOptions()
+    )(children)
+  }
+
+  def apply[T](query: ParsedQuery, queryOptions: QueryOptions)
+              (children: QueryData[T] => ReactElement)
+              (implicit tReader: Reader[T]): slinky.core.BuildingComponent[Element, js.Object] = {
+    apply[T, Unit](
+      query = query,
+      (),
+      queryOptions
+    )(children)
+  }
+
+  def apply[T](query: ParsedQuery)
+              (children: QueryData[T] => ReactElement)
+              (implicit tReader: Reader[T]): slinky.core.BuildingComponent[Element, js.Object] = {
+    apply[T](
+      query = query,
+      queryOptions = QueryOptions()
+    )(children)
+  }
+
+  def apply[Q <: GraphQLQuery](query: Q, variables: Q#Variables, queryOptions: QueryOptions)
+                              (children: QueryData[query.Data] => ReactElement)
+                              (implicit dataReader: Reader[query.Data],
+                               variablesWriter: Writer[Q#Variables]): slinky.core.BuildingComponent[Element, js.Object] = {
+    apply[query.Data, Q#Variables](
       query = query.operation,
-      children = d => {
-        children(queryDataReader.read(d))
-      }
-    ))
+      variables = variables,
+      queryOptions = queryOptions
+    )(children)
   }
 
   def apply[Q <: GraphQLQuery](query: Q, variables: Q#Variables)
                               (children: QueryData[query.Data] => ReactElement)
                               (implicit dataReader: Reader[query.Data],
                                variablesWriter: Writer[Q#Variables]): slinky.core.BuildingComponent[Element, js.Object] = {
-    val queryDataReader = QueryData.reader(dataReader)
-    apply(Props(
-      query = query.operation,
-      variables = variablesWriter.write(variables),
-      children = d => {
-        children(queryDataReader.read(d))
-      }
-    ))
+    apply[Q](
+      query = query,
+      variables = variables,
+      queryOptions = QueryOptions()
+    )(children)
+  }
+
+  def apply(query: GraphQLQuery { type Variables = Unit }, queryOptions: QueryOptions)
+           (children: QueryData[query.Data] => ReactElement)
+           (implicit dataReader: Reader[query.Data]): slinky.core.BuildingComponent[Element, js.Object] = {
+    apply[GraphQLQuery {type Variables = Unit }](
+      query = query,
+      variables = (),
+      queryOptions = queryOptions
+    )(children)
+  }
+
+  def apply(query: GraphQLQuery { type Variables = Unit })
+           (children: QueryData[query.Data] => ReactElement)
+           (implicit dataReader: Reader[query.Data]): slinky.core.BuildingComponent[Element, js.Object] = {
+    apply(
+      query = query,
+      queryOptions = QueryOptions()
+    )(children)
   }
 
   override val component: |[String, js.Object] = ReactApollo.Query
