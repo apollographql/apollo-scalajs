9619ec0b833c73aa8eb9edfc5d6bcb52deff18f7
espinho <cincegan@gmail.com>
2019-03-31 15:43:50 +0100

    Added request and onError to ApolloBoostClientOptions

--- core/src/main/scala/com/apollographql/scalajs/ApolloBoost.scala
+++ core/src/main/scala/com/apollographql/scalajs/ApolloBoost.scala
@@ -1,12 +1,31 @@
 package com.apollographql.scalajs
 
+import com.apollographql.scalajs.link.Operation
 import slinky.readwrite.ObjectOrWritten
 
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSImport
 
 case class ApolloBoostClientOptions(uri: String,
-                                    fetchOptions: js.UndefOr[js.Object] = js.undefined)
+                                    fetchOptions: js.UndefOr[js.Object] = js.undefined,
+                                    request: js.UndefOr[js.Function1[Operation, js.Promise[js.Any]]] = js.undefined,
+                                    onError: js.UndefOr[js.Function1[ApolloError, js.Any]] = js.undefined)
+
+@js.native
+trait ApolloError extends js.Object {
+  val operation: js.Object = js.native
+  val response: js.Object = js.native
+  val graphQLErrors: js.Object = js.native
+  val networkError: ApolloNetworkError = js.native
+}
+
+@js.native
+trait ApolloNetworkError extends js.Object {
+  val name: String = js.native
+  val response: js.Object = js.native
+  val statusCode: Int = js.native
+  val bodyText: String = js.native
+}
 
 @JSImport("apollo-boost", JSImport.Default)
 @js.native
